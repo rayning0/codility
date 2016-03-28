a8ef8b0ce8ac400fd72eadc08387dcc252fd7979
Raymond Gan <rayning@yahoo.com>
2016-03-28 13:34:18 -0700

    Add comments

--- 06-3-max_product_of_3.rb
+++ 06-3-max_product_of_3.rb
@@ -1,6 +1,11 @@
+# https://codility.com/demo/results/trainingTMSNVW-SN5/
+
 def solution(a)
   a.sort!
   len = a.length
+
+  # max product, after sort, is either prod of last 3 numbers,
+  # or prod of first 2 (negative) numbers * last positive number
   [a[0] * a[1] * a[-1], a[len - 3] * a[len - 2] * a[len - 1]].max
 end
 

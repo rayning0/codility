72da09a7dfdfaafd7feb8cf43cf263c67d3a7961
Raymond Gan <rayning@yahoo.com>
2016-03-28 17:16:08 -0700

    Simplify max product of 3 solution

--- 06-3-max_product_of_3.rb
+++ 06-3-max_product_of_3.rb
@@ -1,12 +1,10 @@
-# https://codility.com/demo/results/trainingTMSNVW-SN5/
+# https://codility.com/demo/results/trainingJN2VE5-5Q4/
 
 def solution(a)
   a.sort!
-  len = a.length
-
   # max product, after sort, is either prod of last 3 numbers,
   # or prod of first 2 (negative) numbers * last positive number
-  [a[0] * a[1] * a[-1], a[len - 3] * a[len - 2] * a[len - 1]].max
+  [a[-3] * a[-2] * a[-1], a[0] * a[1] * a[-1]].max
 end
 
 a = [-3, 1, 2, -2, 5, 6]

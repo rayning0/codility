8feb0aa72cfbb388e8203c6e45b10c6bf93e22eb
Raymond Gan <rayning@yahoo.com>
2016-04-10 21:40:37 -0700

    7-1 brackets edit

--- 07-1-brackets.rb
+++ 07-1-brackets.rb
@@ -1,7 +1,6 @@
 # https://codility.com/demo/results/trainingSHP35R-NYT/
 
 def solution(s)
-  return 1 if s == ''
   right_brackets = ['}', ']', ')']
   return 0 if right_brackets.include?(s[0])
   brackets = {'{' => '}', '[' => ']', '(' => ')'}

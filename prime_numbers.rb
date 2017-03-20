24d3fa4cd35767da27d493701d85b1a754f77bd4
Raymond Gan <raymond.y.gan.-nd@disney.com>
2017-03-19 21:58:44 -0700

    Show output of the 3 coins programs

--- prime_numbers.rb
+++ prime_numbers.rb
@@ -48,7 +48,7 @@ end
 # After flipping 10 coins: [H, H, H,...H]
 # We get [T, H, H, T, H, H, H, H, T, H]
 # O(n * n)
-def coins(a)
+def slow_coins(a)
   n = a.size
   (1..n).each do |i|
     c = 1
@@ -62,7 +62,7 @@ def coins(a)
 end
 
 a = ['H'] * 10
-puts "num of tails: #{coins(a)}"
+puts "slow_coins. #{slow_coins(a)} tails"
 
 # O(n log n)
 def faster_coins(n)
@@ -88,7 +88,7 @@ def faster_coins(n)
   tails
 end
 
-puts "final tails: #{faster_coins(100)}"
+puts "faster_coins: #{faster_coins(10)} tails"
 
 # Each coin is flipped over exactly as many times as # of
 # its divisors. The coins flipped an odd number of times show tails,
@@ -105,4 +105,46 @@ def fastest_coins(n)
   Math.sqrt(n).floor
 end
 
-puts "Fastest way: #{fastest_coins(100)} tails."
+puts "fastest_coins: #{fastest_coins(10)} tails"
+
+# slow_coins. 3 tails
+# p: 1, k: 1, coin: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# p: 1, k: 2, coin: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# p: 1, k: 3, coin: [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+# p: 1, k: 4, coin: [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+# p: 1, k: 5, coin: [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+# p: 1, k: 6, coin: [0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+# p: 1, k: 7, coin: [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0]
+# p: 1, k: 8, coin: [0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0]
+# p: 1, k: 9, coin: [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
+# p: 1, k: 10, coin: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+# tails: 1
+# p: 2, k: 2, coin: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+# p: 2, k: 4, coin: [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+# p: 2, k: 6, coin: [0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1]
+# p: 2, k: 8, coin: [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1]
+# p: 2, k: 10, coin: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1]
+# tails: 1
+# p: 3, k: 3, coin: [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+# p: 3, k: 6, coin: [0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0]
+# p: 3, k: 9, coin: [0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0]
+# tails: 1
+# p: 4, k: 4, coin: [0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0]
+# p: 4, k: 8, coin: [0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0]
+# tails: 2
+# p: 5, k: 5, coin: [0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0]
+# p: 5, k: 10, coin: [0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0]
+# tails: 2
+# p: 6, k: 6, coin: [0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1]
+# tails: 2
+# p: 7, k: 7, coin: [0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1]
+# tails: 2
+# p: 8, k: 8, coin: [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1]
+# tails: 2
+# p: 9, k: 9, coin: [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1]
+# tails: 3
+# p: 10, k: 10, coin: [0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1]
+# tails: 3
+# coin: [0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0]
+# faster_coins: 3 tails
+# fastest_coins: 3 tails

f282c2ae47e24f2e79d774c19f48de21f509f91f
Raymond Gan <raymond.y.gan.-nd@disney.com>
2017-03-22 09:19:17 -0700

    Change comments in 9-3-max_profit

--- 09-3-max_profit.rb
+++ 09-3-max_profit.rb
@@ -1,3 +1,4 @@
+# https://codility.com/demo/results/training5EEH22-NT7/
 # a = [25, 23, 30, 20, 16]
 # max_profit = 7
 # profit = -20
@@ -5,9 +6,9 @@
 # min = 16
 # max = 0
 
-# loop: price - all previous prices, then compare to max_profit
-# If we set a new min, we reset max to 0
+# If see a new min, reset max to 0
 
+# O(n)
 def solution(a)
   return 0 if a.empty?
   min = a.shift
@@ -15,11 +16,11 @@ def solution(a)
   profit = max - min
   max_profit = profit
   a.each do |price|
-    if price < min
-      min = price
-      max = 0
-    elsif price > max
+    if price > max
       max = price
+    elsif price < min
+      min = price
+      max = 0 # this price and all later prices won't be used for max_profit, until we see a price > max
     end
     profit = max - min
     max_profit = [max_profit, profit].max

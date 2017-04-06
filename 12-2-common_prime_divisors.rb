8e14b11191020d38ba12581dd64c5a46a64ed361
Raymond Gan <rayning@yahoo.com>
2017-04-05 23:05:59 -0700

    Fast solution for 12-2-common_prime_divisors

--- 12-2-common_prime_divisors.rb
+++ 12-2-common_prime_divisors.rb
@@ -1,3 +1,10 @@
+def gcd(u, v)
+  while v > 0
+    u, v = v, u % v
+  end
+  u
+end
+
 def prime_factors(n)
   f = 2
   factors = []
@@ -20,6 +27,31 @@ def slow_solution(a, b)
     if prime_factors(a[i]).uniq == prime_factors(b[i]).uniq
       count += 1
     end
+  end
+  count
+end
+
+def remove_common_prime_factors(x, y)
+  # Remove all prime factors of x which also in y.
+  # Return extra prime factors of x.
+  while x != 1
+    gcd = gcd(x, y)
+    break if gcd == 1 # x has no more common prime factors
+    x /= gcd # gcd includes all common prime factors of x, y
+  end
+  x
+end
+
+# https://codility.com/demo/results/trainingBUKR3A-FHE/
+# O(Z * log(max(A) + max(B))**2)
+def fast_solution(a, b)
+  count = 0
+  a.zip(b).each do |anum, bnum|
+    gcd = gcd(anum, bnum) # gcd includes ALL common prime factors
+    extra_pf = remove_common_prime_factors(anum, gcd)
+    next if extra_pf != 1 # prime factors of anum, bnum aren't all common
+    extra_pf = remove_common_prime_factors(bnum, gcd)
+    count += 1 if extra_pf == 1
   end
   count
 end

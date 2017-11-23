dee6953fb42de560d4e1072a0b6f83b7230f37fe
Raymond Gan <rayning@yahoo.com>
2017-11-22 21:48:27 -0800

    Add new (similar) solution to 04-3 Missing Integer

--- 04-3-missing_integer.rb
+++ 04-3-missing_integer.rb
@@ -1,4 +1,7 @@
-def solution(a)
+# https://codility.com/demo/results/demoKZNNAB-Q6B/
+require 'rspec/autorun'
+
+def solution1(a)
     n = a.length
     counts = Array.new(n + 1, 0)
     a.each do |num|
@@ -11,4 +14,37 @@ def solution(a)
     end
     n + 1
 end
-p solution([5, 10, 1, 15, 2])
+
+
+# For answer, just care about first n positive integers.
+# Ignore any numbers < 1 or > n.
+
+def solution(a)
+  n = a.size
+  saw = Array.new(n)
+
+  n.times do |i|
+    if a[i].between?(1, n)
+      saw[a[i] - 1] = 1
+    end
+  end
+
+  saw.size.times do |anum|
+    return anum + 1 if saw[anum].nil?
+  end
+
+  n + 1
+end
+
+describe 'Missing Integer' do
+  it 'Finds smallest positive integer that does not occur in a given sequence.' do
+    expect(solution([5])).to eq 1
+    expect(solution([-1000000, 1000000])).to eq 1
+    expect(solution([1, 1, 1])).to eq 2
+    expect(solution([3, 3, 3])).to eq 1
+    expect(solution([-1, -3])).to eq 1
+    expect(solution([1, 2, 3])).to eq 4
+    expect(solution([1, 3, 6, 4, 1, 2])).to eq 5
+    expect(solution([6, 5, 3, 2, 1])).to eq 4
+  end
+end

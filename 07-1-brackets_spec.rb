d3bcef0d53721951dc3e7018324b95daa747bd41
Raymond Gan <rayning@gmail.com>
2020-06-15 16:08:16 -0700

    Add Golang version of Brackets

--- 07-1-brackets_spec.rb
+++ 07-1-brackets_spec.rb
@@ -15,6 +15,36 @@ def solution(s)
   1
 end
 
+# Golang version:
+# https://www.hackerrank.com/challenges/balanced-brackets/problem
+
+# func isBalanced(s string) string {
+#     stack := []string{}
+#     hash := map[string]string{
+#         "(": ")",
+#         "[": "]",
+#         "{": "}",
+#     }
+#     var top string
+
+#     for _, char := range strings.Split(s, "") {
+#         if len(stack) > 0 {
+#             top = stack[len(stack)-1]
+#         } else {
+#             top = ""
+#         }
+#         if hash[top] == char {
+#             stack = stack[:len(stack)-1] // pop off stack
+#         } else {
+#             stack = append(stack, char)
+#         }
+#     }
+#     if len(stack) > 0 {
+#         return "NO"
+#     }
+#     return "YES"
+# }
+
 describe '#brackets' do
   it 'returns 1 if brackets are properly nested' do
     expect(solution("{[()()]}")).to eq 1
